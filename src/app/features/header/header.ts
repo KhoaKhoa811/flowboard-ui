d5a6447568485b6420cb4e15f76b6ab0f20df07f
KhoaKhoa811 <nguyenvoanhkhoa9487@gmail.com>
2026-05-01 22:38:04 +0700

    refactor: implement team details content navigated tabs

--- src/app/features/header/header.ts
+++ src/app/features/header/header.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { HeaderLayoutService } from './../../core/services/header-layout-service';
+import { Component, inject } from '@angular/core';
 import { DefaultAvatar } from '../../shared/components/default-avatar/default-avatar';
 import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [DefaultAvatar, CommonModule],
+  imports: [DefaultAvatar, CommonModule, MatIcon, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  public headerService = inject(HeaderLayoutService);
+}
