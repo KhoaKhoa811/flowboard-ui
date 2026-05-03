e5eef0bd756ea40187cfcea3df532e2b0c2fc42c
KhoaKhoa811 <nguyenvoanhkhoa9487@gmail.com>
2026-05-03 22:36:12 +0700

    refactor: implement team overview header in team details

--- src/app/features/team-details/team-overview/team-overview.ts
+++ src/app/features/team-details/team-overview/team-overview.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NameColorPipe } from '../../../shared/pipes/name-color-pipe';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-team-overview',
-  imports: [],
+  imports: [NameColorPipe, MatIcon],
   templateUrl: './team-overview.html',
   styleUrl: './team-overview.scss',
 })
