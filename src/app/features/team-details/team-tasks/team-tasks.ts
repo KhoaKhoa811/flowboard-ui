14e2b6209c701147aa49d1ea7204ad966f1bdadb
KhoaKhoa811 <nguyenvoanhkhoa9487@gmail.com>
2026-05-05 22:16:49 +0700

    refactor: implement task filters in team task tab in team details section

--- src/app/features/team-details/team-tasks/team-tasks.ts
+++ src/app/features/team-details/team-tasks/team-tasks.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-team-tasks',
-  imports: [],
+  imports: [MatIcon, CommonModule],
   templateUrl: './team-tasks.html',
   styleUrl: './team-tasks.scss',
 })
-export class TeamTasks {}
+export class TeamTasks {
+  filteredName = signal<string>('all');
+
+  onChangeFilter(filterName: string) {
+    this.filteredName.set(filterName);
+  }
+}
